971aa567553f615d02a96ae9068e293b1cf61d3f
Gustavo Ruiz <gustavato978@gmail.com>
2023-05-22 22:51:43 -0300

    dou commit

--- build.sh
+++ build.sh
@@ -2,5 +2,8 @@
 # exit on error
 set -o errexit
 
-py manage.py collectstatic --no-input
-py manage.py migrate
+# poetry install
+# pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
+python manage.py migrate
